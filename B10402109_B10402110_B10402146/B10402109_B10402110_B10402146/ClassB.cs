67e5bc5e6834f9de539007ce45238516d284d50c
kageyazhu <kageyazhu@gmail.com>
2017-11-19 16:10:02 +0800

    problem1
    更新專案上去主幹

---
+++ B10402109_B10402110_B10402146/B10402109_B10402110_B10402146/ClassB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10402109_B10402110_B10402146
+{
+    class ClassB  // 傳入一個數字，傳回是否是質數
+    {
+    }
+}
