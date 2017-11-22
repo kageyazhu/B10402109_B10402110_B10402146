1ff06038624b433eae8eddcef56dc406961ec2d6
B10402146 <b10402146@chu.edu.tw>
2017-11-22 20:24:48 +0800

    problem2
    實作classA的函式(隨機傳回一個0~99的數字)

--- B10402109_B10402110_B10402146/B10402109_B10402110_B10402146/ClassA.cs
+++ B10402109_B10402110_B10402146/B10402109_B10402110_B10402146/ClassA.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Random;
 
 namespace B10402109_B10402110_B10402146
 {
     class ClassA  // 隨機傳回一個0~99的數字
     {
+        public int number(){
+            
+            Random rand = new Random();
+            int a = rand.Next(100);
+            return a;
+        }
+    
     }
 }
