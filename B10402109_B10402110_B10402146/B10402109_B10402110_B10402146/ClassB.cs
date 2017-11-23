877f2ae8873bd749ae00c514dbf52e2e97cffd56
abbieke <33798095+abbieke@users.noreply.github.com>
2017-11-23 17:35:03 +0800

    Update ClassB.cs

--- B10402109_B10402110_B10402146/B10402109_B10402110_B10402146/ClassB.cs
+++ B10402109_B10402110_B10402146/B10402109_B10402110_B10402146/ClassB.cs
@@ -8,5 +8,20 @@ namespace B10402109_B10402110_B10402146
 {
     class ClassB  // 傳入一個數字，傳回是否是質數
     {
+        public int n { get; set; }
+        public int GetNumber()
+        {
+            int i;
+           
+            for (i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                    break;
+            }
+            if (i < n)
+                return 1;
+            else
+                return 0;
+        }
     }
 }
